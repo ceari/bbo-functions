132f076328cd2932a3316a0f8b46f3deba4ff387
Daniel D <daniel.diepold@gmail.com>
2012-08-21 21:37:42 +0200

    R optimization script

---
+++ optimize.R
@@ -0,0 +1,14 @@
+require(DEoptim)
+require(DiceOptim)
+set.seed(1)
+
+
+f = function(x) {
+    val = system(paste("/home/daniel/development/workspace/bbo/bbfunc 123", sample(1:100000, 1), "f110 5 1 1 1 1 1", x[1], x[2], x[3], x[4], x[5]), intern=T)
+    return (as.double(strsplit(val, c(" "))[[1]][[1]]))
+}
+
+est.ras = DEoptim(f, lower=c(-5,-5,-5,-5,-5), upper=c(5,5,5,5,5), control=list(storepopfrom=1, itermax=1000, trace=F))
+
+
+cat (est.ras$optim)
